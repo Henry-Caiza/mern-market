87a081234529e8088afcda90b927860a48a6f6c0
Henry-Caiza <henrypcv1995@gmail.com>
2024-03-10 02:04:28 -0500

    connected db mongo

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB_URL).then(() => {
+    console.log('DB CONNECTED')
+}).catch((err) => {
+    console.error(`Error connecting to the database: ${err}`)
+})
 
 const app = express()
 
