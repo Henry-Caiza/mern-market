cbbd3852c854c16962295b585b08b017322d7cb1
Henry-Caiza <henrypcv1995@gmail.com>
2024-03-10 19:34:00 -0500

    cree el middleware error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
     const hashedPassword = await bcryptjs.hash(password, 10)
     const newUser = new User({ username, email, password: hashedPassword })
@@ -10,9 +11,8 @@ export const signup = async (req, res) => {
         res.status(201).json({
             message: "User created successfully!"
         })
-    } catch (err) {
-        console.log(err.message)
-        return res.status(400).json(err.message)
+    } catch (error) {
+        next(errorHandler(550, "Error creating user"))
     }
 
 }
