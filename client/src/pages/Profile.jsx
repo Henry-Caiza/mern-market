9ffcd94f6c5f55ccea0aa0dcb940998ac013ced3
Henry-Caiza <henrypcv1995@gmail.com>
2024-03-11 18:37:17 -0500

    listing page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react"
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 import { app } from '../firebase'
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess } from "../redux/user/userSlice"
-
+import { Link } from 'react-router-dom'
 
 function Profile() {
     const { currentUser, loading, error } = useSelector((state) => state.user)
@@ -121,6 +121,9 @@ function Profile() {
                 <input type="email" placeholder="email" id="email" className="p-3 border rounded-lg" defaultValue={currentUser.email} onChange={handleChange} />
                 <input type="password" placeholder="password" id="password" className="p-3 border rounded-lg" onChange={handleChange} />
                 <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">{loading ? 'Loading...' : 'Update'}</button>
+                <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to='/create-listing'>
+                    Create Listing
+                </Link>
             </form>
             <div className="flex justify-between mt-5">
                 <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
