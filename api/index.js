fb5cb8e6b71646e9d38ba21910976ea0c055373e
Henry-Caiza <henrypcv1995@gmail.com>
2024-03-11 12:20:53 -0500

    cree user api update

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.routes.js'
+import cookieParese from 'cookie-parser'
 
 dotenv.config()
 
@@ -13,7 +14,9 @@ mongoose.connect(process.env.MONGO_DB_URL).then(() => {
 })
 
 const app = express()
+
 app.use(express.json())
+app.use(cookieParese())
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
