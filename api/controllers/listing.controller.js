1289e0bbdb387fa02518b7aac8d3c25e38d6dfbc
Henry-Caiza <henrypcv1995@gmail.com>
2024-03-12 10:36:14 -0500

    update api and ui

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,9 +1,50 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
     try {
         const listing = await Listing.create(req.body)
         return res.status(201).json(listing)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        return next(errorHandler(404, 'No such listing exists'));
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You do not have permission to perform this action'))
+    }
+    try {
+        await Listing.findByIdAndDelete(req.params.id)
+        return res.status(200).json('Listing Deleted successfully')
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) return next(errorHandler(404, 'No such listing exists'));
+
+    if (req.user.id !== listing.userRef) return next(errorHandler(401, 'You are not authorized to make changes on this'))
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        res.status(200).json(updatedListing)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id)
+        if (!listing) return next(errorHandler(404, "Listing not found"))
+        res.status(200).json(listing)
     } catch (error) {
         next(error)
     }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -113,6 +113,20 @@ function Profile() {
             setShowListingsError(true)
         }
     }
+
+    const handleListingDelete = async (listingId) => {
+        try {
+            const res = await fetch(`/api/listing/delete/${listingId}`, { method: 'DELETE' })
+            const data = await res.json()
+            if (data.success === false) {
+                console.log(data.message)
+                return
+            }
+            setUserListings((prev) => prev.filter((listing) => listing._id !== listingId))
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -164,8 +178,13 @@ function Profile() {
                                     <p className="">{listing.name}</p>
                                 </Link>
                                 <div className="flex flex-col items-center">
-                                    <button className="text-red-700">Delete</button>
-                                    <button className="text-green-700">Edit</button>
+                                    <button
+                                        onClick={() => handleListingDelete(listing._id)}
+                                        className="text-red-700">Delete</button>
+                                    <Link to={`/update-listing/${listing._id}`}>
+                                        <button
+                                            className="text-green-700">Edit</button>
+                                    </Link>
                                 </div>
                             </div>
                         ))}
